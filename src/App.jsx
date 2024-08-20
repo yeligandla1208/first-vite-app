2059e31365832640c21f2e4e989caef0bf3e382c
yeligandla1208 <yeligandlabharath@gmail.com>
2024-08-20 14:38:25 +0530

    date-16-08

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,18 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-// import Clist from './components/lists/list'
-// import { Tables } from './daily_tasks/07-08-task/tablestask'
-// import { Cards } from './daily_tasks/07-08-task/cards'
-// import CustomImage from './components/images/images.jsx'
-import { recipeData } from './data/recipeData.js'
-import { MainHeadings } from './components/headings/heading.jsx'
-import { Ingredients } from './components/Api-data/dataValues.jsx'
-// import { SecondaryHeading } from './components/headings/heading.jsx'
 
+import './App.css'
+import  UserGreeting  from './condtional/condtionalRen.jsx'
 
 
 
 
 function App(){
-//   const flowerListing=[{
-//     name:"flower1",
-//     src: "https://img.freepik.com/premium-photo/flower-sunflower-isolated-yellow-background_198067-526531.jpg",
-//     width: 200,
-//     height:200,
-//   },
-//   {
-//     name:"flower2",
-//     src:"https://img.freepik.com/premium-photo/close-up-bunch-sunflowers-white-background_198067-525954.jpg",
-//     width:250,
-//     height:250,
-//   },
-//   {
-//     name:"flower3",
-//     src:"https://img.freepik.com/premium-photo/painting-flower-with-blue-sky-background-sunflower_198067-526437.jpg",
-//     width:300,
-//     height:300,
-//   },
-//   {
-//     name:"flower2",
-//     src:"https://img.freepik.com/premium-photo/close-up-bunch-sunflowers-white-background_198067-525954.jpg",
-//     width:350,
-//     height:350,
-//   },
-//   {
-//     name:"flower3",
-//     src:"https://img.freepik.com/premium-photo/painting-flower-with-blue-sky-background-sunflower_198067-526437.jpg",
-//     width:400,
-//     height:450,
-//   }
-// ]
-
-
-
-
 
   return (
     <>
-      {/* <CustomImage source="https://img.freepik.com/premium-photo/flower-sunflower-isolated-yellow-background_198067-526531.jpg" alternateText="flower" width={200} height={200}/>
-      <CustomImage source="https://img.freepik.com/premium-photo/close-up-bunch-sunflowers-white-background_198067-525954.jpg" alternateText="flower" width={250} height={250}/>
-      <CustomImage source="https://img.freepik.com/premium-photo/painting-flower-with-blue-sky-background-sunflower_198067-526437.jpg" alternateText="flower" width={300} height={300}/>     */}
-
-
-      {/* {
-      flowerListing.map(eachImage =>{
-        return(
-
-        <CustomImage source={eachImage.src} height={eachImage.height} width={eachImage.width} alternateText={eachImage.name}/>
-        )
-      }) 
-      
-      }*/}
-      {
-        recipeData.map(eachrecipe=>{
-          return(
-            <div>
-              {/* <MainHeadings id={eachrecipe.id}/> */}
-              <MainHeadings heading={eachrecipe.name}/>
-              {/* <SecondaryHeading id={eachresipe.id}/> */}
-              <Ingredients Ingred={eachrecipe.ingredients}/>
-
-             
-             
-
-            </div>
-          )
-        })
-      }
-    
-
+      <UserGreeting isLoggedIn={false} username="Hii"/>
+      {/* <UserGreeting isLoggedIn={false} username="Bharath"/> */}
+      {/* <UserGreeting isLoggedIn={true} username="Bro"/> */}
+      {/* <UserGreeting isLoggedIn={true} username="Hello"/> */}
     </>
     
   )
---
+++ src/condtional/condtionalRen.jsx
@@ -0,0 +1,37 @@
+
+
+/** if and if else  */
+
+// const UserGreeting=(props)=>{
+//     if(props.isLoggedIn){
+//         return(
+//             <h2>Welcome {props.username}</h2>
+//         )
+//     }
+//     else{
+//         return(<h2>Sorry!! please try agin</h2>)
+//     }
+// }
+// export default UserGreeting
+
+
+
+/** ternary operator */
+
+const UserGreeting=(props)=>{
+    return(props.isLoggedIn? <h2>Welcome {props.username}:<h2>please log in to continue</h2></h2>);
+}
+export default UserGreeting
+
+
+
+
+
+
+
+
+
+
+
+
+
